70bdb0ed6f549df9bf558059e416537c9550f655
anub1004 <anubhavsingh6260@gmail.com>
2026-01-09 20:53:03 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Home from "./pages/home/Home";
 import About from "./pages/about/About";
 import Portfolio from "./pages/portfolio/Portfolio";
-import Contact from "./pages/Contact/Contact";
+import Contact from "./pages/contact/Contact";
 import { createBrowserRouter, RouterProvider } from "react-router";
 import AppLayout from "./Components/AppLayout";
 function App() {
